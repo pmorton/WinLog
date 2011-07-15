0bedccdf10f239ea76627b40a6ddbff16f04dd59
Paul Morton <pmorton@biaprotect.com>
2011-07-15 12:22:14 -0700

    Clean Up

---
+++ WinLog/SyslogLevels.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WinLog
+{
+    public enum SyslogLevels
+    {
+            Emergency = 0,
+            Alert = 1,
+            Critical = 2,
+            Error = 3,
+            Warning = 4,
+            Notice = 5,
+            Information = 6,
+            Debug = 7,
+    }
+}
--- WinLog/WinLogService.cs
+++ WinLog/WinLogService.cs
@@ -22,12 +22,17 @@ namespace WinLog
 
         protected override void OnStart(string[] args)
         {
-           
+            
+            Logger logger = LogManager.GetLogger("WinLog");
+            logger.Info("WinLog is starting...");
+
             //Handle Event Log Events
             EventLog[] mlogs = EventLog.GetEventLogs();
+            logger.Debug("Retrived {0} log(s) to register with EntryWrittenHandeler", mlogs.Length);
 
             foreach (EventLog log in mlogs)
             {
+                logger.Debug("Registering '{0}' event log with EntryWrittenHandeler", log.LogDisplayName.ToString());
                 log.EnableRaisingEvents = true;
                 log.EntryWritten += new EntryWrittenEventHandler(this.EntryHandeler);
             }
@@ -36,45 +41,67 @@ namespace WinLog
 
         void EntryHandeler(object sender, EntryWrittenEventArgs e)
         {
-            // Transform EventLog Severity from Windows to Syslog
+            // Transform EventLog Severity from Windows to Syslog and NLog Levels
             LogLevel level;
+            SyslogLevels syslogLevel;
             switch (e.Entry.EntryType)
             {
                 case (EventLogEntryType.Information):
-                    { level = LogLevel.Info; break; }
+                    { level = LogLevel.Info; syslogLevel = SyslogLevels.Information; break; }
                 case (EventLogEntryType.Warning):
-                    { level = LogLevel.Warn; break; }
+                    { level = LogLevel.Warn; syslogLevel = SyslogLevels.Warning; break; }
                 case (EventLogEntryType.Error):
-                    {level = LogLevel.Error; break; }
+                    { level = LogLevel.Error; syslogLevel = SyslogLevels.Error; break; }
                 case (EventLogEntryType.SuccessAudit):
-                    { level = LogLevel.Trace; break; }
+                    { level = LogLevel.Debug; syslogLevel = SyslogLevels.Debug; break; }
                 case (EventLogEntryType.FailureAudit):
-                    { level = LogLevel.Info; break; }
+                    { level = LogLevel.Warn; syslogLevel = SyslogLevels.Warning; break; }
                 default:
-                    { level = LogLevel.Debug; break; }
+                    { level = LogLevel.Debug; syslogLevel = SyslogLevels.Debug; break; }
             }
 
-           
+            
 
-            string message = System.String.Format("EventID:{0} {1}",
-                                                    e.Entry.EventID,
-                                                    e.Entry.Message.Replace("\r", " ").Replace("\n", " ").Replace("\t", " ") );
+           
+            //Clean the message up to remote new lines.
+            string message = e.Entry.Message.Replace("\r", " ").Replace("\n", " ").Replace("\t", " ") ;
+
+            //Transform the event to be sent.
+            string username;
+            if (System.String.IsNullOrEmpty(e.Entry.UserName)) { username = ""; } else { username = e.Entry.UserName; }
 
             WindowsEventStub stubEvent = new WindowsEventStub { Category = e.Entry.Category, 
                                                                 CategoryNumber = e.Entry.CategoryNumber.ToString(),
                                                                 EntryType = e.Entry.EntryType.ToString(),
-                                                                EventID = e.Entry.EventID.ToString(),
+                                                                EventID = e.Entry.InstanceId.ToString(),
                                                                 MachineName = e.Entry.MachineName,
                                                                 Message = message,
                                                                 Source = e.Entry.Source,
                                                                 TimeGenerated = e.Entry.TimeGenerated.ToUniversalTime().ToString(),
                                                                 TimeWritten = e.Entry.TimeWritten.ToUniversalTime().ToString(),
-                                                                UserName = e.Entry.UserName};
-            
+                                                                UserName = username};
+
+
+            //Create a syslog message string
+            string sysLogMessage = System.String.Format("<{0}>{1} {2} {3}[{4}]: {5}",
+                             17 * 8 + syslogLevel, 
+                             e.Entry.TimeGenerated.ToUniversalTime().ToString("MMM dd HH:mm:ss"), 
+                             e.Entry.MachineName,
+                             e.Entry.Source.Replace(" ", "-"),
+                             e.Entry.InstanceId.ToString(), 
+                             message);
+
+
+            //Generate an NLog Event Object
             Logger log = LogManager.GetCurrentClassLogger();
-            LogEventInfo theEvent = new LogEventInfo(level, e.Entry.Source.Replace(" ", "-") , message );
-            
+            LogEventInfo theEvent = new LogEventInfo(level, e.Entry.Source.Replace(" ", "-"), message);
+
+            //Add JSON properties to the event
             theEvent.Properties["JSON"] = JsonConvert.SerializeObject(stubEvent);
+
+            //Add a formatted Syslog event to the string
+            theEvent.Properties["SYSLOG"] = sysLogMessage;
+
             log.Log(theEvent);
 
         }
